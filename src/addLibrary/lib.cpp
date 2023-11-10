cb9a42c162116616bf8f82d305dc22a541550879
Mohamed Sayyaff <sayyaff@users.noreply.github.com>
2023-11-10 19:19:38 +0530

    Add files via upload

---
+++ src/addLibrary/lib.cpp
@@ -0,0 +1,9 @@
+#include "lib.h"
+
+AddTwoNumbers::AddTwoNumbers() {
+    // Constructor implementation (if needed)
+}
+
+int AddTwoNumbers::add(int a, int b) {
+    return a + b;
+}
---
+++ src/addLibrary/lib.h
@@ -0,0 +1,10 @@
+#ifndef ADD_TWO_NUMBERS_H
+#define ADD_TWO_NUMBERS_H
+
+class AddTwoNumbers {
+public:
+    AddTwoNumbers();  // Constructor
+    int add(int a, int b);
+};
+
+#endif // ADD_TWO_NUMBERS_H
